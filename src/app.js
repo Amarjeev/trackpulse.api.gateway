cb3d474f51ebe526a2f6cf38feef76b7cf3922c0
amarjeev ms <amarjeevms@gmail.com>
2026-04-21 19:59:57 +0530

    update CORS configuration to use environment variable for client URL

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import { logger } from "./middlewares/logger.js";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // your frontend
+  origin: process.env.CLIENT_URL, // your frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
