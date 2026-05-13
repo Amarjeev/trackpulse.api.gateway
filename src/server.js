687efa05863e5aeb0514540cad693c22fad31749
amarjeev ms <amarjeevms@gmail.com>
2026-05-14 01:09:52 +0530

    update server to listen on all network interfaces

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 API Gateway running on port ${PORT}`);
 });
