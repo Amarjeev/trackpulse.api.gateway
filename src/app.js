11b44a56ebc6e62e3725bf4a409682de03f45dc9
amarjeev ms <amarjeevms@gmail.com>
2026-05-17 21:11:14 +0530

    feat: initialize Express application with CORS, cookie parsing, and gateway routes

--- src/app.js
+++ src/app.js
@@ -6,33 +6,33 @@ import { logger } from "./middlewares/logger.js";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// }));
 
-// const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.CLIENT_URL];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
+app.use(
+  cors({
+    origin: function (origin, callback) {
 
-//       // Allow mobile apps / Postman / no-origin requests
-//       if (!origin) return callback(null, true);
+      // Allow mobile apps / Postman / no-origin requests
+      if (!origin) return callback(null, true);
 
-//       if (allowedOrigins.includes(origin)) {
-//         return callback(null, true);
-//       }
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
 
-//       return callback(new Error("CORS blocked"));
-//     },
+      return callback(new Error("CORS blocked"));
+    },
 
-//     methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
 
-//     credentials: true,
-//   })
-// );
+    credentials: true,
+  })
+);
 
 // app.use(express.json());
 app.use(cookieParser());
